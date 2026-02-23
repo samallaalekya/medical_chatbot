3ca2f85d8a9eabd22891a1e800593c117c2ee593
samallaalekya <samallaalekya@gmail.com>
2026-02-23 19:35:21 +0530

    first commit

---
+++ template.sh
@@ -0,0 +1,15 @@
+#creating directory
+mkdir -p src
+mkdir -p research
+
+#creating files
+touch src/_init__.py
+touch src/helpers.py
+touch src/prompts.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and files created successfully!"
